a7bdb28034e0dfc052b8f17cb2bcb1b3d185d417
jhonson-lab-win <jhonsonlaid@gmail.com>
2017-05-08 20:08:24 +0800

    format douyu-login.py

--- douyu-login.py
+++ douyu-login.py
@@ -2,21 +2,24 @@
 import urllib
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import bs4
+# import bs4
 import time
 
+sina_account = "jhonsonlai@sina.com"
+sina_passwd = "pwd"
+douyu_login_url = ("https://api.weibo.com/oauth2/authorize?client_id="
+                   "979098171&redirect_uri=http://www.douyu.com"
+                   "/member/oauth/signin/weibo")
 
-def get_page(url):
-    page = urllib.urlopen(url)
-    content = page.read()
-    return content
+chrome_path = ("C:/Program Files (x86)/Google/"
+               "Chrome/Application/chromedriver.exe")
 
 
-douyu_login_url = "https://api.weibo.com/oauth2/authorize?client_id=979098171&\
-redirect_uri=http://www.douyu.com/member/oauth/signin/weibo"
+# def get_page(url):
+#     page = urllib.urlopen(url)
+#     content = page.read()
+#     return content
 
-chrome_path = "C:/Program Files (x86)/Google/\
-Chrome/Application/chromedriver.exe"
 
 # page_content = get_page(douyu_login_url)
 # print page_content.decode("utf-8")
@@ -24,9 +27,9 @@ Chrome/Application/chromedriver.exe"
 browser = webdriver.Chrome(chrome_path)
 browser.get(douyu_login_url)
 account_elem = browser.find_element_by_id('userId')
-account_elem.send_keys('user@sina.com')
+account_elem.send_keys(sina_account)
 passwd_elem = browser.find_element_by_id('passwd')
-passwd_elem.send_keys('pwd')
+passwd_elem.send_keys(sina_passwd)
 time.sleep(0.2)
 submit_elem = browser.find_element_by_class_name("WB_btn_login")
 submit_elem.click()
